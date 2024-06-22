39461ae8f338e4c7cd50b600d95ace1378dbc271
1Bira <ubiratan.gleidson@gmail.com>
2024-06-22 03:09:55 +0100

    verifu topalerts

--- src/utils/AlertMetrics.ts
+++ src/utils/AlertMetrics.ts
@@ -55,6 +55,9 @@ export const AlertsMetrics = (
   //get Top 10 by criticality
   const openAlerts = alerts.filter((a) => a.state === "open");
   const top10Alerts = openAlerts.sort(compareAlertSeverity); //.slice(0, 10);
+  core.info(`top10Alerts: `+ top10Alerts.length);
+
+  
 
   openedLastXDays = openAlerts.filter(
     (a: CodeScanningAlert | DependencyOrCodeAlert | SecretScanningAlert) => {
